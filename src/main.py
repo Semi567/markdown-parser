6c3b03bed0dd1cc5f83e607324623314df1b08cb
Semi56 <swiety56@gmail.com>
2025-02-06 14:02:59 +0100

    beginning

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+    text_node_one = TextNode("This is a bold text", TextType.BOLD, "https://www.boot.dev")
+    print(text_node_one)
+
+main()
