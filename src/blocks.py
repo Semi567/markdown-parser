4c85f633a5e02a4802b6b414ff277cb7d3927c7d
Semi56 <swiety56@gmail.com>
2025-02-14 10:45:17 +0100

    block to block_type

--- src/blocks.py
+++ src/blocks.py
@@ -6,3 +6,50 @@ def markdown_to_blocks(markdown):
             continue
         stripped_list_of_blocks.append(el.strip())
     return stripped_list_of_blocks
+
+def block_to_block_type(block):
+    if (block.startswith('# ') 
+        or block.startswith('## ') 
+        or block.startswith('### ') 
+        or block.startswith('#### ') 
+        or block.startswith('##### ')
+        or block.startswith('###### ')):
+        return "heading"
+    elif block.startswith("```") and block.endswith("```"):
+        return "code"
+    elif block.startswith(">"):
+        lines = block.split("\n")
+        for line in lines:
+            if line.startswith(">"):
+                continue
+            else:
+                return "paragraph"
+        return "quote"
+    elif block.startswith("- "):
+        lines = block.split("\n")
+        for line in lines:
+            if line.startswith("- "):
+                continue
+            else:
+                return "paragraph"
+        return "unordered_list"
+    elif block.startswith("* "):
+        lines = block.split("\n")
+        for line in lines:
+            if line.startswith("* "):
+                continue
+            else:
+                return "paragraph"
+        return "unordered_list"
+    elif block.startswith("1. "):
+        lines = block.split("\n")
+        i = 1
+        for line in lines:
+            if line.startswith(f"{i}. "):
+                i += 1
+                continue
+            else:
+                return "paragraph"
+        return "ordered_list"
+    else:
+        return "paragraph"
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,7 +1,9 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type
 
 class TestBlocks(unittest.TestCase):
+    #TEST MARKDOWN TO BLOCKS
+
     def test_markdown_to_blocks(self):
         markdown = """ # This is a heading
 
@@ -17,4 +19,85 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                 'This is a paragraph of text. It has some **bold** and *italic* words inside of it.', 
                 '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
             ]
+        )
+
+    # TEST BLOCK_TO_BLOCK_TYPE
+
+    def test_block_to_block_type_paragraph(self):
+        markdown = "This is a simple paragraph"
+        self.assertEqual(
+            block_to_block_type(markdown),
+            "paragraph"
+        )
+
+    def test_block_to_block_type_ordered_list(self):
+        markdown = "1. This is first line\n2. This is second line"
+        self.assertEqual(
+            block_to_block_type(markdown),
+            "ordered_list"
+        )
+    
+    def test_block_to_block_type_unordered_list(self):
+        markdown = "* This is first line\n* This is second line"
+        self.assertEqual(
+            block_to_block_type(markdown),
+            "unordered_list"
+        )
+    
+    def test_block_to_block_type_fake_list(self):
+        markdown = "* This is first line\n- This is second line"
+        self.assertEqual(
+            block_to_block_type(markdown),
+            "paragraph"
+        )
+    
+    def test_block_to_block_type_heading(self):
+        markdown = "# This is a simple paragraph"
+        markdown_two = "## text"
+        markdown_three = "####### fake"
+        self.assertEqual(
+            block_to_block_type(markdown),
+            "heading"
+        )
+        self.assertEqual(
+            block_to_block_type(markdown_two),
+            "heading"
+        )
+        self.assertEqual(
+            block_to_block_type(markdown_three),
+            "paragraph"
+        )
+    
+    def test_block_to_block_type_code(self):
+        markdown = "``` This is a simple paragraph ```"
+        markdown_two = "``` text"
+        markdown_three = "``fake``"
+        self.assertEqual(
+            block_to_block_type(markdown),
+            "code"
+        )
+        self.assertEqual(
+            block_to_block_type(markdown_two),
+            "paragraph"
+        )
+        self.assertEqual(
+            block_to_block_type(markdown_three),
+            "paragraph"
+        )
+    
+    def test_block_to_block_type_quote(self):
+        markdown = "> Line one\n> Line two"
+        markdown_two = "> Line one\nLine two"
+        markdown_three = "> Just one line"
+        self.assertEqual(
+            block_to_block_type(markdown),
+            "quote"
+        )
+        self.assertEqual(
+            block_to_block_type(markdown_two),
+            "paragraph"
+        )
+        self.assertEqual(
+            block_to_block_type(markdown_three),
+            "quote"
         )
